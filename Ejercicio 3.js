b59f029d40ff3d2581f16fbc5996bcc6f3899c92
juligarciadevweb <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-14 20:45:59 -0300

    Programa que saluda al usuario

---
+++ Ejercicio 3.js
@@ -0,0 +1,4 @@
+//Escribir un programa que pregunte al usuario su nombre y luego lo salude
+
+let nombre = prompt("¿What is your name? ")
+document.write("Your name is " + nombre + " pleased to meet you")
