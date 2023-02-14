fff492987caa5852a8242fb24c933fc1ee4f7846
juligarciadevweb <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-14 20:44:03 -0300

    Programa para convertir pesos argentinos a dolares

---
+++ Ejercicio 2.js
@@ -0,0 +1,16 @@
+//Programa para convertir pesos argentinos a dolares
+    //Chat GPT solution
+        let tasaDeCambio = 3500; // Tasa de cambio fija de pesos a dólares
+        let pesos = prompt("Ingrese la cantidad de pesos que desea convertir:");
+        let dolares = pesos / tasaDeCambio;
+        console.log("El valor en dólares es: " + dolares);
+    
+    //Algoritmo propio
+
+    let mn = prompt("Ingrese la cantidad de pesos ") //Mn seria moneda nacional
+    let dolar = 347
+    let cMn = mn/347
+
+    document.write("Tus " + mn + " pesos, equivalen a " + cMn + " dolares")
+
+//
