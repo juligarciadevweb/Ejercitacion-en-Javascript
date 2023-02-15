e14662564e5eb4e186f62e81541c56911f91a2eb
juligarciadevweb <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-14 21:04:57 -0300

    //Mostrar el 20% de un numero

---
+++ Ejercicio 4.js
@@ -0,0 +1,13 @@
+//Ingresar el nro entero y mostrar el 20% del mismo
+
+let number = prompt("Ingrese el numero: ")
+const porcentaje = 20;
+let result = (porcentaje/100)*number
+
+document.write("El 20% de " + number + " es " + result)//Ingresar el nro entero y mostrar el 20% del mismo
+
+let number = prompt("Ingrese el numero: ")
+const porcentaje = 20;
+let result = (porcentaje/100)*number
+
+document.write("El 20% de " + number + " es " + result)
