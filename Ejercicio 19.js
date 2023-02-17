28c470cdbb560ad956461fb055956e8522da3b02
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-17 12:43:56 -0300

    Múltiplo de X

---
+++ Ejercicio 19.js
@@ -0,0 +1,19 @@
+/*
+
+Realizar un programa que pida al usuario un número y le indique si es múltiplo de 3 o no.
+Cuando decimos que un número es múltiplo de otro, 
+significa que podemos obtener el primer número al multiplicar el segundo número por algún entero.
+
+*/
+
+let number = parseInt(prompt("Ingrese el numero"))
+
+if (number / 3 === 0) {
+    console.log("Es multiplo de 3")
+} else {
+    console.log("No es multiplo de 3")
+}
+
+
+
+
