bb50f1819f66b448704583c6ea4243861168e75b
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-20 17:01:10 -0300

    Conducción con IF

---
+++ Ejercicio 025.js
@@ -0,0 +1,11 @@
+  /*
+	Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+*/
+
+let edad = parseInt(prompt("Cual es tu edad?"))
+
+if (edad >= 18) {
+	console.log("Puedes conducir")
+} else {
+	console.log("No puedes conducir")
+}
