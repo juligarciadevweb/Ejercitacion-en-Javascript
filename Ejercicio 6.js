b063bd3831f2921f4716a07d7641d16af947e56d
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-04-15 19:53:49 -0300

    Delete Ejercicio 6.js

--- Ejercicio 6.js
+++
@@ -1,9 +0,0 @@
-// Calcular el area y perimetro de un rectangulo
-
-let l1 = prompt("Ingrese el ancho del rectangulo ")
-let l2 = prompt("Ingrese la altura del rectangulo ")
-
-let area = l1 * l2 //Calculo de area
-let perimetro = (l1*2) + (l2*2) //Calculo de perimetro
-
-document.write("El area es " + area + " mientras que el perimetro es " + perimetro)
