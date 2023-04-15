65bfffc27755ca54698a52b0a85636ab11ae3d35
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-04-15 19:50:55 -0300

    Delete Ejercicio 023.js

--- Ejercicio 023.js
+++
@@ -1,8 +0,0 @@
-// Ingresar 3 numeros y mostrar el promedio
-
-let num1 = parseInt(prompt("Ingresa el 1er numero"))
-let num2 = parseInt(prompt("Ingresa el 2do numero"))
-let num3 = parseInt(prompt("Ingresa el 3er numero"))
-
-let promedio = (num1 + num2 + num3) / 3 
-console.log("El promedio es " + promedio)
