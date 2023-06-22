90a7ea5764724e1b50047bc168516af82acce1c6
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-06-22 13:20:46 -0300

    Do while y mayor
    En un colegio se ingresa la calificación final obtenida por cada alumno de un curso. El proceso se repite mientras la calificación final ingresada sea menor a 10.Finalizado el ingreso deberá mostrar el promedio de las notas ingresadas y cual fue la mayor nota obtenida.

---
+++ Ejercicio 96.js
@@ -0,0 +1,28 @@
+/*
+En un colegio se ingresa la calificación final obtenida por cada alumno de un curso. 
+El proceso se repite mientras la calificación final ingresada sea menor a 10.
+Finalizado el ingreso deberá mostrar el promedio de las notas ingresadas 
+y cual fue la mayor nota obtenida.
+*/
+let nota
+let mayor = 0
+let suma = 0
+let x = 0
+
+    do 
+    {   
+       nota = parseInt(prompt("Ingrese la nota"))
+       //No debes usar la palabra reservada let, porque ya esta declarada previamente
+       suma = suma + nota
+       //Usamos una compuerta logica clasica para asegurarnos de que la nota se mayor a 0
+       if (nota != 0 && suma > mayor) 
+        {
+            mayor = nota
+        } 
+        x++
+    } while (nota != 0); 
+
+let promedio = suma / x
+console.log("El promedio de las notas es "+promedio)
+console.log("--")
+console.log("La nota mayor es "+mayor)
